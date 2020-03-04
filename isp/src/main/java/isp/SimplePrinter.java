9f8e6302fde20a54c5e9b92ade173a83fae804a0
manaleelgaamouss <61496979+manaleelgaamouss@users.noreply.github.com>
2020-03-04 23:00:28 +0100

    rectif classe simpeprinter

--- isp/src/main/java/isp/SimplePrinter.java
+++ isp/src/main/java/isp/SimplePrinter.java
@@ -1,23 +1,8 @@
 package isp;
 
-public class SimplePrinter implements Printer,Scan,Fax,Copy {
+public class SimplePrinter implements Printer {
 
 	public void print() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void scan() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void fax() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void copy() {
 		// TODO Auto-generated method stub
 		
 	}
